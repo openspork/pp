c861dc233ca0012d1d80f328475e0aeea4de7428
openspork <bobo@fraplin.fun>
2019-05-08 22:29:33 -0700

    getting closer

--- create_tables.py
+++ create_tables.py
@@ -1,43 +1,42 @@
-#import pymysql
-# db=pymysql.connect('localhost','pp','password','pp')
-# cursor = db.cursor()
-# cursor.execute('select user from user;')
-# data = cursor.fetchone()
-from peewee import *
 import xmltodict
+import pymysql
 
-mysql_db = MySQLDatabase('pp', user = 'pp', password = 'password', host = 'localhost', port = 3316)
+db = pymysql.connect('localhost','pp','password','pp')
+cursor = db.cursor()
 
-class BaseModel(Model):
-	class Meta:
-		database = mysql_db
-
-class Leaf(BaseModel)
-
+# key is table name, value is [(key, value), (key, value)]
+data = {}
 
 
 
+def create_tables(data):
+	for table in data.keys():
+		columns = []
+		for leaf_pair in data[table]:
+
+			columns.append(leaf_pair[0])
+
+		print('creating table %s\ncolumns: %s' % (table, columns))
 
 
 
-def create_table():
-	print('creating table')
 
+def find_leaf(parent, d):
+	global data
+	leaf_pairs = []
+	for key, value in d.items():
+		if ( isinstance(value,dict) ):
+			find_leaf(key, value)
+		else:	
+			#print('found leaf -- parent: %s key: %s value: %s' % (parent, key, value))
+			leaf_pairs.append((key, value))
+
+	data[parent] = leaf_pairs
 
 with open('./configs/site.cfg') as fd:
 	content = fd.read()
 	doc = xmltodict.parse(content)
 
-	def find_leaf(parent, d):
-		# search children
-		for key, value in d.items():
-			if ( isinstance(value,dict) ):
-				#print('found a dict!')
-				#print('searching child: %s' % key )
-				find_leaf(key, value)
-			else:	
-				print('found leaf -- parent: %s key: %s value: %s' % (parent, key, value))
-
-
 	find_leaf(None, doc)
 
+create_tables(data)
