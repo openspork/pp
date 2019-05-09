c1a85d8d26c9be962283a2a9f5de8225102825c9
christian <christianrgerlach@gmail.com>
2019-05-09 14:27:33 -0700

    cleaned up creation string gen

--- create_tables.py
+++ create_tables.py
@@ -8,6 +8,31 @@ cursor = db.cursor()
 data = {}
 
 def create_tables(data):
+    # Define static tables
+    create_strings = [
+    'CREATE TABLE IF NOT EXISTS Phones (ID INT NOT NULL, PRIMARY KEY(ID), MACAddress VARCHAR(255) NOT NULL)',
+    'CREATE TABLE IF NOT EXISTS Clients (ID INT NOT NULL, PRIMARY KEY(ID), `Name` VARCHAR(255) NOT NULL)',
+    'CREATE TABLE IF NOT EXISTS Sites (ID INT NOT NULL, PRIMARY KEY(ID), `Name` VARCHAR(255) NOT NULL)',
+    'CREATE TABLE IF NOT EXISTS Models (ID INT NOT NULL, PRIMARY KEY(ID), `Name` VARCHAR(255) NOT NULL)',
+    'CREATE TABLE IF NOT EXISTS Expansions (ID INT NOT NULL, PRIMARY KEY(ID), `Name` VARCHAR(255) NOT NULL)'
+    ]
+    # Create static tables
+    for create_string in create_strings:
+        cursor.execute(create_string)
+
+    # Begin our creation string
+    create_string = 'CREATE TABLE IF NOT EXISTS '
+    primary_key = 'ID INT NOT NULL,PRIMARY KEY (ID)'
+    phone_key = 'PhoneID INT, FOREIGN KEY (PhoneID) REFERENCES Phones(ID)'
+    client_key = 'ClientID INT, FOREIGN KEY (ClientID) REFERENCES Clients(ID)'
+    site_key = 'SiteID INT, FOREIGN KEY (SiteID) REFERENCES Sites(ID)'
+    expansion_key = 'ExpansionID INT, FOREIGN KEY (ExpansionID) REFERENCES Expansions(ID)'
+    parent_key = 'ParentID INT, FOREIGN KEY (ExpansionID) REFERENCES Expansions(ID)'
+    keys_string = '%s,%s,%s,%s,%s' % (primary_key, phone_key, client_key, site_key, expansion_key)
+
+    # Begin our insertion string
+    insert_string = 'REPLACE INTO '
+
     for table in data.keys():
         if ( table ):
             columns = []
@@ -16,22 +41,16 @@ def create_tables(data):
                 columns.append(leaf_pair[0])
                 rows.append(leaf_pair[1])
 
+            # Only process if there are columns to create
             if ( len(columns) > 0 ):
                 #print('creating table %s\ncolumns: %s' % (table, columns))
-
-                primary_key = 'ID INT NOT NULL,PRIMARY KEY (ID)'
-                phone_key = 'PhoneID INT, FOREIGN KEY (PhoneID) REFERENCES Phones(ID)'
-                client_key = 'ClientID INT, FOREIGN KEY (ClientID) REFERENCES Clients(ID)'
-                site_key = 'SiteID INT, FOREIGN KEY (SiteID) REFERENCES Sites(ID)'
-
-                keys_string = '%s,%s,%s,%s' % (primary_key, phone_key, client_key, site_key)
-
-                create_string = 'CREATE TABLE IF NOT EXISTS `%s`(%s,' % (table, keys_string)
+                create_string = 'CREATE TABLE IF NOT EXISTS '
+                create_string += '`%s`(%s,' % (table, keys_string)
                 for column in columns:
                     create_string += '`%s`TEXT,' % column
                 create_string = create_string[:-1]
                 create_string += ')'
-                print(create_string)
+                #print(create_string,'\n')
                 cursor.execute(create_string)
 
                 # Now input data into the freshly created tables
@@ -52,16 +71,15 @@ def create_tables(data):
                 row_string += ')'
 
                 replace_string = 'REPLACE INTO `%s` %s VALUES %s' % (table, column_string, row_string)
-                print(replace_string)
-                #cursor.execute(replace_string)
-
-
+                #print(replace_string,'\n')
+                cursor.execute(replace_string)
 
 def find_leaf(parent, d):
     global data
     leaf_pairs = []
     for key, value in d.items():
         if ( isinstance(value,dict) ):
+            # Create an intermediate node
             find_leaf(key, value)
         else:   
             #print('found leaf -- parent: %s key: %s value: %s' % (parent, key, value))
