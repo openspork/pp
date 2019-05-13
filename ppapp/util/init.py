b6166c5e9a6e5841fb0938d891b0aed90e81234d
openspork <bobo@fraplin.fun>
2019-05-12 18:52:07 -0700

    dynamically create tables

--- ppapp/util/init.py
+++ ppapp/util/init.py
@@ -4,21 +4,25 @@ from ppapp.models import *
 def init_db():
     print('db init')
     db.connect()
-    db.create_tables([Phone,
-    				Client,
-    				Site,
-    				Extension,
-                    PhoneClients,
-                    PhoneSites,
-                    PhoneExtensions,
-    				ParamLevel,
-    				BaseParam,
-    				AvailParam,
-    				AvailParamPhones,
-    				AvailParamClients,
-    				AvailParamSites,
-    				AvailParamExtensions],
-    				safe = True)
+
+    desired_tables = [Phone,
+                Client,
+                Site,
+                Extension,
+                PhoneClients,
+                PhoneSites,
+                PhoneExtensions,
+                ParamLevel,
+                BaseParam,
+                AvailParam,
+                AvailParamPhones,
+                AvailParamClients,
+                AvailParamSites,
+                AvailParamExtensions]
+
+    for desired_table in desired_tables:
+        db.create_tables(desired_table)
+
     if ( len(ParamLevel.select()) == 0 ):
         print('populating paramlevels')
         build_params()
