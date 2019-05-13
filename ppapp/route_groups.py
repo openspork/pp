9bc01084d3ec8b648dadc2b25bdf929bb721315d
openspork <bobo@fraplin.fun>
2019-05-12 23:18:24 -0700

    adding groups workingw flash

--- ppapp/route_groups.py
+++ ppapp/route_groups.py
@@ -9,8 +9,8 @@ def new_group():
     if request.method == 'POST':   
         if form.validate_on_submit():
             grouptype = GroupType.get(GroupType.id == form.type.data)
-            group = Group.create(name = form.name.data, type = grouptype).save()
-            #flash('New Group - Name: {}, Type: {}'.format(form.name.data, group.type.name))
+            group = Group.create(name = form.name.data, type = grouptype, note = form.note.data).save()
+            flash('New Group - Name: {}, Type: {}'.format(form.name.data, grouptype.name))
             return redirect('/')
         else:
             flash('Invalid Input!')
