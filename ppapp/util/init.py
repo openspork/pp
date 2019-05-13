b0ffef9ee1643030654f590f591d56899ee59712
christian <christianrgerlach@gmail.com>
2019-05-13 14:18:26 -0700

    added default group types

--- ppapp/util/init.py
+++ ppapp/util/init.py
@@ -15,6 +15,28 @@ def init_db():
     				AvailParamPhones,
     				AvailParamGroups],
     				safe = True)
+
+    # Create our group types
+    query = GroupType.select().where(GroupType.name == 'Client')
+
+    if not query.exists():
+    	GroupType.create(name = 'Client', note = 'Group for client level config')
+
+    query = GroupType.select().where(GroupType.name == 'Site')
+
+    if not query.exists():
+    	GroupType.create(name = 'Site', note = 'Group for site level config')
+
+    query = GroupType.select().where(GroupType.name == 'Model')
+
+    if not query.exists():
+    	GroupType.create(name = 'Model', note = 'Group for phone model level config')
+
+    query = GroupType.select().where(GroupType.name == 'Addon')
+
+    if not query.exists():
+    	GroupType.create(name = 'Addon', note = 'Group for addon unit level config')
+
     if ( len(ParamLevel.select()) == 0 ):
         print('populating paramlevels')
         build_params()
