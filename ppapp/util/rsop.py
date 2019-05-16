1ac7ce123204da63c8fbb6e008a5a253d535249f
christian <christianrgerlach@gmail.com>
2019-05-15 19:41:14 -0700

    type precedence working

--- ppapp/util/rsop.py
+++ ppapp/util/rsop.py
@@ -8,12 +8,29 @@ def drill(group, rsop, depth):
     params = get_group_params(group)[1]
 
     for param in params:
+        # If this is not a duplicate
         if param.base_param.name not in rsop.keys():
+            # Add to our dict
             rsop[param.base_param.name] = (param.value, group, depth, [])
         else:
-            print('dupe!  Appending override!', (param.value, group.id, depth))
-            rsop[param.base_param.name][3].append((param.value, group.id, depth))
-            
+            # TODO: add logic to prefer based on type
+            # Compare depth of saved param to current
+            if depth > rsop[param.base_param.name][2] :
+                # If deeper, add as an override
+                print('Deeper dupe!')
+                rsop[param.base_param.name][3].append((param.value, group.id, depth))
+            elif rsop[param.base_param.name][2] == depth:
+                # If equal, prefer based on group type precedence
+                existing_group = Group.get(Group.id == rsop[param.base_param.name][1])
+                # If the new type is greater than previous, update
+                if group.type.precedence > existing_group.type.precedence:
+                    print('Higher priority dupe!')
+                    overridden_param_overrides = rsop[param.base_param.name][3] # Save existing overrides
+                    overridden_param = (rsop[param.base_param.name][0], rsop[param.base_param.name][1].id, rsop[param.base_param.name][2])
+                    overridden_param_overrides.append(overridden_param) # Concatenate previously overridden params with current
+                    rsop[param.base_param.name] = (param.value, group, depth, overridden_param_overrides) # Update param
+                elif group.type.precedence == existing_group.type.precedence:
+                    raise Exception('Duplicate param of equal precedence!  Cannot resolve!')
 
     groups = get_group_groups(group,'parents')[1]
     if len(groups) == 0:
