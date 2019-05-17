4e5be7fb159949258a91869ea22e26536f5083b4
christian <christianrgerlach@gmail.com>
2019-05-17 15:38:30 -0700

    working on recursively finding param roots

--- ppapp/util/gen_xml.py
+++ ppapp/util/gen_xml.py
@@ -2,8 +2,44 @@ from peewee import *
 from playhouse.shortcuts import *
 from ppapp.models import *
 
-def get_dict():
-	pass
+def get_branch_dict(params):
+	# Create a dict of dicts with each ParamLevel as key
+	param_levels = {}
+	for param in params:
+		param_level = param[0]
+		base_param = param[1]
+		avail_param_value = param[2]
+		print('\nProcessing param_level: "%s" base_param: "%s" avail_value: "%s"' % (param_level.name, base_param.name, avail_param_value))
+		
+		param_levels[param_level] = {} # Initiate empty dict
+		print('Array created for param level: "%s"' % param_level.name )
+		# Populate the arrays
+		for param2 in params:
+			# if param level is the key, append it to the array
+			param_level2 = param2[0]
+			if param_level == param_level2:
+				base_param = param2[1]
+				param_value = param2[2]
+				param_levels[param_level][base_param] = param_value # Add our value with base as key
+
+	return param_levels
+
+def find_root(param_branch_key):
+
+	parent = (ParamLevel
+			.select()
+			# If we are looking for parents, we are matching on child
+			.join(ParamLevelParamLevels, JOIN.LEFT_OUTER, on = (ParamLevelParamLevels.child == ParamLevel.id))
+			.where(ParamLevel)
+			)
+
+	# If we have a parent node, recurse further
+	if param_branch_key.parent:
+		find_root(param_branch_key.parent)
+	else:
+		print('Found root node: %s' % param_branch_key)
+
+
 
 def gen_xml(rsop):
 	# Build an array of (ParamLevel, BaseParam, avail_param_value) tuples
@@ -16,23 +52,10 @@ def gen_xml(rsop):
 		tup = (param_level, base_param, avail_param_value)
 		params.append(tup)
 
-	# Create a dict of arrays with each ParamLevel as key
-	param_levels = {}
-	for param in params:
-		param_level = param[0]
-		base_param = param[1]
-		avail_param_value = param[2]
-		print('\nProcessing param_level: "%s" base_param: "%s" avail_value: "%s"' % (param_level.name, base_param.name, avail_param_value))
-		
-		param_levels[param_level] = [] # Initiate empty array
-		print('Array created for param level: "%s"' % param_level.name )
-		# Populate the arrays
-		for param2 in params:
-			# if param level is the key, append it to the array
-			param_level2 = param2[0]
-			if param_level == param_level2:
-				base_param = param2[1]
-				param_value = param2[2]
-				param_levels[param_level].append(param_value)
+		param_branches = get_branch_dict(params)
 
-	print(param_levels.keys())
+		# for each param level, replace it with full surrounding dict
+		for param_branch_key in param_branches.keys():
+			find_root(param_branch_key)
+
+
