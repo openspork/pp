8e03ba535b2c5f3ae01fda5d309705f5b8b870fb
christian <christianrgerlach@gmail.com>
2019-05-17 13:46:24 -0700

    starting work on gen xml, need to fix creating param levels properly

--- ppapp/util/gen_xml.py
+++ ppapp/util/gen_xml.py
@@ -6,9 +6,33 @@ def get_dict():
 	pass
 
 def gen_xml(rsop):
+	# Build an array of (ParamLevel, BaseParam, avail_param_value) tuples
 	params = []
-	for param in rsop:
-		print(param)
-		#params.append((param[0], param[1]))
-	print(rsop)
-	print(params)
+	#print(rsop)
+	for base_param_id, param_value in rsop.items():
+		base_param = BaseParam.get(BaseParam.id == base_param_id)
+		param_level = BaseParam.param_level
+		avail_param_value = param_value[0]
+		tup = (param_level, base_param, avail_param_value)
+		params.append(tup)
+
+	# Create a dict of arrays with each ParamLevel as key
+	param_levels = {}
+	for param in params:
+		param_level = param[0]
+		base_param = param[1]
+		avail_param_value = param[2]
+		print('\nProcessing param_level: %s base_param: %s avail_value: %s' % (param_level.name, base_param.name, avail_param_value))
+		
+		param_levels[param_level] = [] # Initiate empty array
+		print('Array created for param level: %s' % param_level.name )
+		# Populate the arrays
+		for param2 in params:
+			# if param level is the key, append it to the array
+			param_level2 = param2[0]
+			if param_level == param_level2:
+				base_param = param2[1]
+				param_value = param2[2]
+				param_levels[param_level].append(param_value)
+
+	print(param_levels.keys())
