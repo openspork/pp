494f38ffd60cabf66718032562dcec53460e36ad
christian <christianrgerlach@gmail.com>
2019-05-17 15:00:14 -0700

    fix genner

--- ppapp/util/gen_xml.py
+++ ppapp/util/gen_xml.py
@@ -11,7 +11,7 @@ def gen_xml(rsop):
 	#print(rsop)
 	for base_param_id, param_value in rsop.items():
 		base_param = BaseParam.get(BaseParam.id == base_param_id)
-		param_level = BaseParam.param_level
+		param_level = base_param.param_level
 		avail_param_value = param_value[0]
 		tup = (param_level, base_param, avail_param_value)
 		params.append(tup)
@@ -22,10 +22,10 @@ def gen_xml(rsop):
 		param_level = param[0]
 		base_param = param[1]
 		avail_param_value = param[2]
-		print('\nProcessing param_level: %s base_param: %s avail_value: %s' % (param_level.name, base_param.name, avail_param_value))
+		print('\nProcessing param_level: "%s" base_param: "%s" avail_value: "%s"' % (param_level.name, base_param.name, avail_param_value))
 		
 		param_levels[param_level] = [] # Initiate empty array
-		print('Array created for param level: %s' % param_level.name )
+		print('Array created for param level: "%s"' % param_level.name )
 		# Populate the arrays
 		for param2 in params:
 			# if param level is the key, append it to the array
