c5466f8ce9e5594a0ed22d735f2edf280cc09711
openspork <bobo@fraplin.fun>
2019-05-19 00:43:52 -0700

    i thihk i made soemthing work

--- ppapp/app.py
+++ ppapp/app.py
@@ -6,6 +6,6 @@ from ppapp.util.group_ops import get_phone_groups, get_group_groups
 app = Flask(__name__)
 app.config.from_object(Config)
 app.add_template_global(get_phone_groups)
-app.add_template_global(get_phone_groups)
+app.add_template_global(get_group_groups)
 
 db = MySQLDatabase('pp', user='pp', password='password', host='localhost', port=3306)
