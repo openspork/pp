ba74361653556f14981ce4ebc0f52f5e1dccd492
openspork <bobo@fraplin.fun>
2019-05-18 19:59:41 -0700

    holy fuck worky?

--- ppapp/util/gen_xml.py
+++ ppapp/util/gen_xml.py
@@ -4,7 +4,7 @@ from ppapp.models import *
 #import xml.dom.minidom
 import xmltodict
 
-def get_branch_dict(params):
+def get_branch_dict_by_model(params):
     # Create a dict of dicts with each ParamLevel as key
     param_levels = {}
     for param in params:
@@ -26,18 +26,34 @@ def get_branch_dict(params):
 
     return param_levels
 
+def get_branch_dict_by_name(params):
+    # Create a dict of dicts with each ParamLevel name as key
+    param_levels = {}
+    for param in params:
+        param_level = param[0].name
+        base_param = param[1].name
+        avail_param_value = param[2]
+        #print('\nProcessing param_level: "%s" base_param: "%s" avail_value: "%s"' % (param_level, base_param, avail_param_value))
+        
+        param_levels[param_level] = {} # Initiate empty dict
+        #print('Array created for param level: "%s"' % param_level.name )
+        # Populate the arrays
+        for param2 in params:
+            # if param level is the key, append it to the array
+            param_level2 = param2[0].name
+            if param_level == param_level2:
+                base_param = param2[1].name
+                param_value = param2[2]
+                param_levels[param_level][base_param] = param_value # Add our value with base as key
+
+    return param_levels
+
     # TODO: convert Models into strings
 def sanitize_dict(param_branch):
-    print(param_branch.name)
-    # Fetch our edge param level
-    #param_level = ParamLevel.get(ParamLevel == param_branch)
-    # Print our avail params
-    #print(param_level.name)
-
-
+    pass
 
 # "current" - is a ParamLevel
-def build_dict(current, param_branch):
+def build_dict(current, raw_param_branch):
     #print(f'Found root node {current.name}')
     temp_dict = {}
     query = (ParamLevel
@@ -50,19 +66,15 @@ def build_dict(current, param_branch):
     while query.exists():
         i += 1
         result = query.get()
-        temp_dict = {result.name: temp_dict}
-        if i == 1:
-            temp_dict[current] = sanitize_dict(current)
+        temp_dict = {result.name: raw_param_branch}
         query = (ParamLevel
             .select()
             # If we are looking for parents, we are matching on child
             .join(ParamLevelParamLevels, JOIN.LEFT_OUTER, on = (ParamLevelParamLevels.parent == ParamLevel.id))
             .where(ParamLevelParamLevels.child == result)
             )
+    print('our parent tree: %s' % temp_dict)
     return temp_dict
-
-
-
 
 def gen_xml(rsop):
     # Build an array of (ParamLevel, BaseParam, avail_param_value) tuples
@@ -76,16 +88,23 @@ def gen_xml(rsop):
         tup = (param_level, base_param, avail_param_value)
         params.append(tup)
 
-        param_branches = get_branch_dict(params)
-
+        param_branches = get_branch_dict_by_model(params)
+        param_branches_by_name = get_branch_dict_by_name(params)
         
         # for each param level, build a pure dict
         for param_branch_key, param_branch in param_branches.items():
-            # Create the surrounding dict with our branch appended
-            branch_dict = build_dict(param_branch_key, param_branch) 
+            print('processing branch: %s' % param_branch_key.name)
+            
+            # get our raw branch (no Models)
+            raw_param_branch = param_branches_by_name[param_branch_key.name]
+            
+            print('our raw param branch: %s' % raw_param_branch)
 
-            #xml_string = dicttoxml(branch_dict['polycomConfig'], root=False, attr_type=False).decode('utf-8')
-            #dom = xml.dom.minidom.parseString(xml_string).toprettyxml()
+            # Create the surrounding dict with our raw branch appended
+            full_branch_dict = build_dict(param_branch_key, raw_param_branch) 
+
+            print('our complete dict: %s' % full_branch_dict)
+
             #xml_string = xmltodict.unparse(branch_dict, pretty = True)
             xml_string = ''
             xmls.append(xml_string)
