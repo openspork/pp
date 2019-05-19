ed02842eb896a1a2371c7579c920e49c81d5f72e
openspork <bobo@fraplin.fun>
2019-05-18 22:59:37 -0700

    showing groups for phones on index

--- ppapp/app.py
+++ ppapp/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from peewee import MySQLDatabase
 from config import Config
+from ppapp.util.group_ops import get_phone_groups, get_group_groups
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.add_template_global(get_phone_groups)
+app.add_template_global(get_phone_groups)
 
 db = MySQLDatabase('pp', user='pp', password='password', host='localhost', port=3306)
