5d05ad94c69468c61350a6a153c49a3e62001350
christian <christianrgerlach@gmail.com>
2019-05-22 17:26:02 -0700

    fix CA override not showing

--- ppapp/routes/route_logs.py
+++ ppapp/routes/route_logs.py
@@ -5,19 +5,20 @@ from ppapp.models import *
 from ppapp.util.misc import format_mac
 
 
-@app.route('/poly/<mac_address>-<type>.<ext>', methods=['GET', 'HEAD', 'PUT'])
-def put_log(mac_address, type, ext):
+@app.route('/poly/<dir>/<mac_address>-<type>.<ext>', methods=['GET', 'HEAD', 'PUT'])
+def put_log(mac_address, dir, type, ext):
     formatted_mac_address = format_mac(mac_address)
     if request.method =='PUT':
         query = Phone.select().where(Phone.mac_address == formatted_mac_address)
         if query.exists():
             phone = Phone.get(Phone.mac_address == formatted_mac_address)
-            if type == 'boot':
-                BootLog.create(phone = phone, date_time = datetime.now(), data=request.data)
-            elif type == 'app':
-                AppLog.create(phone = phone, date_time = datetime.now(), data=request.data)
-            elif type == 'calls':
-                CallLog.create(phone = phone, date_time = datetime.now(), data=request.data)
+            if dir == 'logs':
+                if type == 'boot':
+                    BootLog.create(phone = phone, date_time = datetime.now(), data=request.data)
+                elif type == 'app':
+                    AppLog.create(phone = phone, date_time = datetime.now(), data=request.data)
+                elif type == 'calls':
+                    CallLog.create(phone = phone, date_time = datetime.now(), data=request.data)
         else:
             app.logger.warn('Failed to find phone: %s' % formatted_mac_address)
             return abort(403)
